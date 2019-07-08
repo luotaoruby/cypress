622d1b84816f51561e1095f76908a7f1357d8520
luotao <luotao.ruby@gmail.com>
2019-07-08 14:33:01 +0800

    Fixup test case.

--- cypress/integration/widgets/mortgage_rates_calculator.spec.js
+++ cypress/integration/widgets/mortgage_rates_calculator.spec.js
@@ -3,11 +3,54 @@ context('MortgageRates Calculator', () => {
     cy.visit('/mortgage-rates/a/mortgage-rate-calculator', {failOnStatusCode: false})
   })
 
-  it('show calculator', () => {
-    cy.get('#ml-calc-v2').
-      should('contain', 'Mortgage Calculator').
-      should('contain', 'Your Home Price:').
-      find('button.next').should('contain', 'Next')
+  it('should works.', () => {
+    cy.get('#ml-calc-v2').should('be.exist').
+      should('contain', 'Mortgage Calculator')
 
+    // Step 1
+    cy.get('#ml-calc-v2').
+      contains('Your Home Price: $200,000').should('be.visible')
+
+    cy.get('#ml-calc-v2').
+      contains('Your Down Payment:$40,000').should('not.visible')
+
+    cy.get('#ml-calc-v2').
+      find('button.previous').should('not.visible')
+
+    cy.get('#ml-calc-v2').
+      find('button.next').should('be.visible').click()
+
+    // Step 2
+    cy.get('#ml-calc-v2 button.previous').should('be.visible')
+
+    cy.get('#ml-calc-v2').
+      contains('Your Home Price: $200,00').should('not.visible')
+
+    cy.get('#ml-calc-v2').
+      contains('Your Down Payment:$40,000').should('be.visible')
+
+    cy.get('#ml-calc-v2').
+      find('button.next').should('be.visible').click()
+
+    // Step 3
+    cy.get('#ml-calc-v2').
+      contains('Preferred Loan Term (Fixed Rate)').should('be.visible')
+
+    cy.get('#ml-calc-v2').
+      contains('15 years').should('be.visible')
+    cy.get('#ml-calc-v2').
+      contains('20 years').should('be.visible')
+    cy.get('#ml-calc-v2').
+      contains('30 years').should('be.visible')
+
+    cy.get('#ml-calc-v2').
+      find('button.next').should('be.visible').click()
+
+    // Step 4
+    cy.get('#ml-calc-v2').
+      contains('Preliminary Monthly Payment Estimate: $911').should('be.visible')
+
+    cy.get('#ml-calc-v2').
+      contains('Improve Your Payment').should('be.visible')
   })
 })
